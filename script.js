071839e6dc2442ccd9a2bb364f9551ea78a4d459
Janetta Kataoeva <jkattoeva@gmail.com>
2022-02-25 15:51:17 +0600

    do homework

---
+++ script.js
@@ -0,0 +1,26 @@
+let article = document.getElementById('article');
+article.style.display = "none";
+document.getElementById('art1').addEventListener('click',function(){
+  article.style.display = "block";
+});
+document.getElementById('art1').addEventListener('dblclick',function(){
+  article.style.display = "none";
+});
+
+let article2 = document.getElementById('article2');
+article2.style.display = "none";
+document.getElementById('art2').addEventListener('click',function(){
+  article2.style.display = "block";
+});
+document.getElementById('art2').addEventListener('dblclick',function(){
+  article2.style.display = "none";
+});
+
+let article3 = document.getElementById('article3');
+article3.style.display = "none";
+document.getElementById('art3').addEventListener('click',function(){
+  article3.style.display = "block";
+});
+document.getElementById('art3').addEventListener('dblclick',function(){
+  article3.style.display = "none";
+});
